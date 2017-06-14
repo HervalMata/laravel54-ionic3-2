cb03d3c0882e038de06d9760cf6ceb0e1fe35054
Carlos Anders <carlosanders@gmail.com>
2017-06-13 22:31:11 -0300

    Aula 57 - Config files test e storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,11 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
+        //aula 54
+        'videos_local' =>[
+            'driver' => 'local',
+            'root' => storage_path('app/videos_test'),
+        ],
 
         's3' => [
             'driver' => 's3',
