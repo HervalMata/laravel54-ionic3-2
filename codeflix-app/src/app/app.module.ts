c0667ce9c809256809d6b618c172e4a3341a97bd
Carlos Anders <carlosanders@gmail.com>
2017-09-14 22:07:28 -0300

    12.1 - metodo getToken JWT

--- codeflix-app/src/app/app.module.ts
+++ codeflix-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { HttpModule } from "@angular/http";
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {HttpModule} from "@angular/http";
+import {IonicStorageModule} from "@ionic/storage";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { LoginPage } from "../pages/login/login";
-import { JwtClientProvider } from '../providers/jwt-client/jwt-client';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {LoginPage} from "../pages/login/login";
+import {JwtClientProvider} from '../providers/jwt-client/jwt-client';
+
 
 
 @NgModule({
@@ -24,6 +26,7 @@ import { JwtClientProvider } from '../providers/jwt-client/jwt-client';
     HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({driverOrder: ['localstorage']})
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -39,4 +42,5 @@ import { JwtClientProvider } from '../providers/jwt-client/jwt-client';
     JwtClientProvider
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- codeflix-app/src/providers/jwt-client/jwt-client.ts
+++ codeflix-app/src/providers/jwt-client/jwt-client.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Storage} from "@ionic/storage";
+
 import {JwtCredentials} from "../../models/jwt-credentials";
 
 /*
@@ -12,8 +14,27 @@ import {JwtCredentials} from "../../models/jwt-credentials";
 @Injectable()
 export class JwtClientProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello JwtClientProvider Provider');
+  private _token = null;
+
+  constructor(public http: Http, public storage:Storage) {
+    this.getToken().then((token)=>{
+      console.log(token);
+    });
+  }
+
+  getToken(): Promise<string>{
+
+    return new Promise((resolve) => {
+      if(this._token){
+        resolve(this._token);
+      }
+      this.storage.get('token').then((token) => {
+        this._token = token;
+        resolve(this._token);
+      });
+    });
+
+
   }
 
   access_token(jwtCredentials: JwtCredentials): Promise<string>{
@@ -23,6 +44,9 @@ export class JwtClientProvider {
       .then((response: Response) =>{
         //console.log(response);
         let token = response.json().token;
+        this._token = token;
+        this.storage.set('token', this._token);
+
         return token;
       });
   }
